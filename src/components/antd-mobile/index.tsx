7c0046efb3fe9466dd0cc27d83000aa686e8d73a
Zhong Jun <45657181@qq.com>
2020-02-20 23:22:37 +0800

    1、增加围产保健导航列表 2、增加Touchable组件

--- src/components/antd-mobile/index.tsx
+++ src/components/antd-mobile/index.tsx
@@ -1,9 +1,11 @@
 import Button from './Button';
 import List from './List';
-import WhiteSpace from './WhiteSpace'
+import WhiteSpace from './WhiteSpace';
+import Touchable from './Touchable';
 
 export {
   Button,
   List,
   WhiteSpace,
+  Touchable,
 }
