caea237445b406be6363c5d74df91c7b73fb2728
zwj7181 <45657181@qq.com>
2020-02-27 13:33:43 +0800

    banner显示

--- src/pages/home/Banner.tsx
+++ src/pages/home/Banner.tsx
@@ -8,7 +8,7 @@
  */
 
 import * as React from 'react';
-
+import { KG } from '@/utils/utils';
 import styles from './Banner.less';
 
 interface IProps {
@@ -17,15 +17,46 @@ interface IProps {
 }
 export default (props: IProps) => {
   const {
-    dataSource: { thumbnail = 'default', name, gesweek, hospital },
+    dataSource: { thumbnail = 'default', name, hospital, adjustedEdd, edd, lmp, gestationalWeek },
   } = props;
+  // 无法取到预产期时，ps:末次月经时间一定存在 ps: 修订预产期 || 孕产期 || 根据末次月经计算的孕产期
+  let EDD = adjustedEdd || edd || KG.getEdd(lmp);
+  // 孕周 ps: 孕周 || 计算的孕周
+  const gesweek = gestationalWeek || KG.getGesweek(EDD);
+  // 距离预产期天数
+  const days = KG.getGesdays(EDD);
+  // banner背景图
+  const bannerbg = (gesweek: string) => {
+    // gesweek的格式存在可能 1. ’20+1‘ 2. ’20周+1天‘
+    let week: number = parseInt(gesweek);
+    if (week < 0) return 'M0';
+    if (week < 4) return 'M1';
+    if (week < 6) return 'M2';
+    if (week < 12) return 'M3';
+    if (week < 16) return 'M4';
+    if (week < 20) return 'M4';
+    if (week < 24) return 'M5';
+    if (week < 28) return 'M6';
+    if (week < 32) return 'M7';
+    if (week < 33) return 'M7-1';
+    if (week < 35) return 'M8';
+    if (week < 36) return 'M8-1';
+    if (week < 37) return 'M9';
+    if (week <= 38) return 'M9-1';
+    if (week >= 39) return 'M10';
+  }
   return (
     <div className={styles.banner}>
       <div className={styles.innerBanner}>
-        <img alt="banner" src={require(`../../assets/baby/${thumbnail}.png`)} />
+        <img alt="banner" src={require(`../../assets/baby/${bannerbg(gesweek)}.png`)} />
         <div className={styles.userinfo}>
           <div className={styles.name}>{name}</div>
-          <div className={styles.gesweek}>{gesweek}</div>
+          <div className={styles.gesweek}>孕 {gesweek} 周</div>
+          <div className={styles.gext}>
+            <div>距预产期 {EDD}</div>
+            <div>还有 {days} 天</div>
+          </div>
+
           <div className={styles.hospital}>{hospital}</div>
         </div>
       </div>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -117,6 +117,123 @@ export function generateUUID(len: number, radix: number) {
   return uuid.join('');
 }
 
+/**
+ * 日期获取，格式化、孕期相关计算
+ */
+export const KG = {
+  getDate() {
+    const nowTimeStamp = Date.now();
+    const now = new Date(nowTimeStamp);
+    const y: number = now.getFullYear();
+    let m: number | string = now.getMonth() + 1;
+    let d: number | string = now.getDate();
+    if (m < 10) {
+      m = '0' + '' + m;
+    }
+    if (d < 10) {
+      d = '0' + '' + d;
+    }
+    return y + '-' + m + '-' + d;
+  },
+
+  /**
+   *
+   *
+   * @param {string} date 字符串时间，2010-10-02
+   * @returns
+   */
+  formatDate(date: string) {
+    const arraydate = date.split("-");
+    if (arraydate.length < 3) {
+      return date
+    }
+    return arraydate[0] + "年" + arraydate[1] + "月" + arraydate[2] + "日";
+  },
+
+  /**
+   * 计算今天距离预产期天数
+   *
+   * @param {string} edd 预产期
+   * @returns
+   */
+  getGesdays(edd: string) {
+    const now = new Date();
+    const remainingTime = new Date(edd).getTime() - now.getTime();
+    let days = 0;
+    if (remainingTime > 0) {
+      days = Math.floor(remainingTime / (24 * 3600 * 1000));
+    }
+    return days;
+  },
+
+  /**
+   * 获取孕产期时间
+   *
+   * @param {string} lmp 末次月经日期
+   * @returns {string}
+   */
+  getEdd(lmp: string) {
+    if (!lmp) {
+      return;
+    }
+    let timeStamp = new Date(lmp).getTime();
+    timeStamp = timeStamp + 280 * 24 * 60 * 60 * 1000;
+    // 标准孕期时间
+    const gestationTime = new Date(timeStamp);
+    const y = gestationTime.getFullYear();
+    let m: number | string = gestationTime.getMonth() + 1;
+    let d: number | string = gestationTime.getDate();
+    if (m < 10) {
+      m = '0' + m;
+    }
+    if (d < 10) {
+      // eslint-disable-next-line no-useless-concat
+      d = '0' + d;
+    }
+    return y + '-' + m + '-' + d;
+  },
+
+  /**
+   * 计算当天孕周
+   *
+   * @param {string} edd 孕产期
+   * @returns {string}
+   */
+  getGesweek(edd: string) {
+    if (!edd) {
+      return;
+    }
+    const remainingTime = new Date(edd).getTime() - new Date().getTime();
+    let yunzh = '';
+    if (remainingTime > 0) {
+      let days = 280 - Math.floor(remainingTime / (24 * 60 * 60 * 1000));
+      if (days > 0) {
+        const week = Math.floor(days / 7);
+        let day: number | string = Math.floor(days % 7);
+        if (day === 0) {
+          day = '周';
+        } else {
+          day = '+' + day;
+        }
+        yunzh = week + day;
+      }
+    } else {
+      let days = Math.floor(Math.abs(remainingTime / (24 * 60 * 60 * 1000)));
+      const week = Math.floor(days / 7);
+      if (week >= 2) {
+        yunzh = '42';
+      } else {
+        let day: number | string = Math.floor(days % 7);
+        if (day === 0) {
+          day = '';
+        }
+        yunzh = week + 40 + '+' + day;
+      }
+    }
+    return yunzh;
+  }
+};
+
 // 跳转
 export function router(type: string) {
   if (!type) {
