1bb502c9bfc6be260b3b9dd1fe6f807d8e68fedf
zwj7181 <45657181@qq.com>
2020-03-09 19:00:17 +0800

    血糖赋值取值结构化约定

--- src/pages/tools/weight/Input.tsx
+++ src/pages/tools/weight/Input.tsx
@@ -4,16 +4,18 @@
  * @Description: 体重输入
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'dva';
+import moment from 'moment';
+import { Toast } from 'antd-mobile';
 
 import DatePicker from '../components/DatePicker';
 import { ConnectState } from '@/models/connect';
 import BackButton from '@/components/BackButton';
-import { IconFont, Button } from '@/components/antd-mobile';
+import { IconFont, Button, WhiteSpace } from '@/components/antd-mobile';
 import GaugeInput from '../components/GaugeInput';
 import { router } from '@/utils/utils';
-import styles from './styles.less';
+import styles from '../blood-pressure/Input.less';
 
 const nowTimeStamp = Date.now();
 const now = new Date(nowTimeStamp);
@@ -26,11 +28,20 @@ interface IProps {
 }
 
 function WeightInput() {
-  const [date, setDate] = React.useState(now);
+  const [date, setDate] = useState(now);
+  const [weight, setWeight] = useState();
 
   React.useEffect(() => {
 
-  }, [])
+  }, []);
+
+  const onSubmit = () => {
+    const d = moment(date).format('YYYY-MM-DD');
+    if (!weight) {
+      return Toast.info('请输入体重数值...');
+    }
+    console.log({ d, weight });
+  }
 
   return (
     <div className={styles.container}>
@@ -43,15 +54,29 @@ function WeightInput() {
         value={date}
         onChange={date => setDate(date)}
       />
+      <WhiteSpace />
       <div className={styles.content}>
         <div className={styles.text} onClick={() => router('/weight/record')}>
-          <IconFont type="icon_article_line" size="34px" />
-          历史记录
+          <IconFont type="record" size="28px" />
+          <span>历史记录</span>
         </div>
-        <GaugeInput id="weight" title="体重" suffix="kg" min={30} max={80} />
+        {/* <GaugeInput id="weight" title="体重" suffix="kg" min={30} max={80} /> */}
+        <div className={styles.circle}>
+          <span>体重</span>
+          <div className={styles.input}>
+            <input
+              type="number"
+              placeholder="输入..."
+              value={weight}
+              onChange={e => setWeight(Number(e.target.value))}
+            />
+            <IconFont type="editor-line" size="0.36rem" />
+          </div>
+          <span className={styles.unit}>kg</span>
+        </div>
       </div>
       <div style={{ margin: '.5rem .3rem' }}>
-        <Button type="primary">保存</Button>
+        <Button type="primary" onClick={onSubmit}>保存</Button>
         <BackButton />
       </div>
     </div>
